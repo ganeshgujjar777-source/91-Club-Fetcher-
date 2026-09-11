3920300c77ad4d6dadc76431d641529d7e7f35c1
ganeshgujjar777-source <ganeshgujjar777@gmail.com>
2026-09-11 18:19:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ module.exports = async (req, res) => {
         : {
             "pageIndex": 1,
             "pageSize": 10,
-            "optionId": "308"
+            "optionId": "201"
         };
 
     const executeRequest = async (authTkn, xAuthTkn) => {
